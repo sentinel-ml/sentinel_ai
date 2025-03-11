5d9e1a13fbbf304c1984dd8145f5df9eb91e7d1c
TeddyMuli <tmuli974@gmail.com>
2025-03-11 10:03:14 +0300

    create TransactionData model

--- api/server.py
+++ api/server.py
@@ -14,7 +14,10 @@ class TransactionData(BaseModel):
     distance_from_home: float
     distance_from_last_transaction: float
     ratio_to_median_purchase_price: float
-
+    repeat_retailer: float
+    used_chip: float
+    used_pin_number: float
+    online_order: float
 
 @app.get('/', methods=['GET'])
 def index():
